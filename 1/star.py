d0145f0fca931ee9d8549014484a7c7879583770
fords <zxw6805@rit.edu>
2015-05-03 09:50:21 +0100

    add star.py

---
+++ 1/star.py
@@ -0,0 +1,56 @@
+""" draw star
+
+    file : star.py
+    author : zeyar Win
+    day : 9 September 2011
+    """
+from turtle import *
+def triangle():
+    """
+    draw  one triangle 
+    """
+    left(60)
+    forward(20)
+    right(120)
+    forward(20)
+    right(120)
+    forward(20)
+    
+def manyTriangle():
+    """
+    draw totlal 5 triangles
+    """
+    seth(0)
+    forward(20)
+    right(72)
+    triangle()
+    left(180)
+    forward(20)
+    right(72)
+    triangle()
+    left(180)
+    forward(20)
+    right(72)
+    triangle()
+    left(180)
+    forward(20)
+    right(72)
+    triangle()
+    
+    
+    
+
+def main():
+    """
+     The program waits for the user to respond to end the program"""
+    triangle()
+    manyTriangle()
+   
+    input ("Hit enter to end program")
+    
+    
+    # begins run program here
+    
+speed(0)
+main()
+    
