e352b3e502103ddce34140bfb659e76f1b8574df
fords <zxw6805@rit.edu>
2015-05-08 09:18:38 +0100

    square python

---
+++ 2/squares_simple.py
@@ -0,0 +1,41 @@
+"""
+   Drawing many 3 quarter squares
+   file Squrares_simpley.py
+   author : Zeyar Win
+"""
+from turtle import *
+
+d=int(input("Enter depth"))
+
+def initialization():
+    """
+     
+     set the window canvas size
+
+    """
+    setup(600,600)
+    pensize(2)
+    speed(0)
+    
+    setworldcoordinates(-202,-202,202,202)
+  
+
+  
+
+      
+    
+
+def main():
+    """ wait for the user to press enter to end the program 
+
+    """
+    initialization()
+
+
+
+# runs program from here
+
+main()
+ 
+    
+    
