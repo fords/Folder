47e0de9621ebc9584fb21c00df4cf2b750f7e63a
fords <zxw6805@rit.edu>
2015-07-12 11:13:17 +0100

    pack.py putbin function

--- Python/6/pack.py
+++ Python/6/pack.py
@@ -30,10 +30,55 @@ def readPut(size,name):
     
     return blosize
 
+def descPutFit(size,bin1,block,row,col):
+    """check the blocks can be fitted into bin """
+    if row+block>size:
+        return False
+    elif col+block>size:
+        return False
+    else:
+        for x in range(row,row+block):
+            for y in range(col,col+block):
+                if bin1[x][y]!=0:
+                    return False
+        return True
+
+def findOpenSpot(size,bin1,i,unpacked):
+    """put the blocks in the available spaces and can show the final state of bin
+        freespace and unpacked """
+    Freespace=0
+    for r in range(len(bin1)):
+        for c in range(len(bin1)):
+            if descPutFit(size,bin1,int(i),r,c)==True:
+                bin1,Freespace=putBin(size,bin1,i,r,c)
+                check(bin1)
+                return bin1,Freespace,unpacked
+
+
+unpacked.append(i)
+return bin1,Freespace,unpacked
+
+
+
+def putBin(size,bin1,block,row,col):
+    """put the blocks into the bin and count the free space  """
+    for r in range(row, row+block):
+        for c in range(col,col+block):
+            bin1[r][c]=block
+    
+    count=0
+    for r in range(len(bin1)):
+        for c in range(len(bin1)):
+            if bin1[r][c]==0:
+                count+= 1
+    return bin1 ,count
 
 
                 
 def main():
-    print('Hello')
+    size=int(input("Enter the size of bins"))
+    file=input("Enter the filename")
+    bin1=createBin(size)
+    block=readPut(size,file)
     
 main()
